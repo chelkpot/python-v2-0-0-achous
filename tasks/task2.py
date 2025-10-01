2f05b6c9fb320c9e9ac32a8dbaba0eb494c1328e
student <student@corp.kpiot.ru>
2025-10-01 15:29:17 +0500

    fafasgfsash

--- tasks/task2.py
+++ tasks/task2.py
@@ -3,10 +3,10 @@
 def solve():
 # Ниже пишите решение задачи
     x,y,z = map(int, input().split())
-price_pencil = 3
-price_pen = price_pencil + 2
-price_marker = price_pen + 7  
-total = (x * price_pencil) + (y * price_pen) + (z * price_marker)
+    price_pencil = 3
+    price_pen = price_pencil + 2
+    price_marker = price_pen + 7  
+    total = (x * price_pencil) + (y * price_pen) + (z * price_marker)
 
 print(total)
    
